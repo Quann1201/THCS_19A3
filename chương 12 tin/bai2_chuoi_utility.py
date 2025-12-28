ec760fcfb5a7538f3d231d13f59e227a36bc8344
Quann1201 <nvquan.25174600166@sv.uneti.edu.vn>
2025-12-28 18:22:29 +0700

    Delete chương 12 tin directory

--- chương 12 tin/bai2_chuoi_utility.py
+++
@@ -1,6 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-
-def dem_so_tu(chuoi):
-    # tách chuỗi theo khoảng trắng và đếm số phần tử
-    return len(chuoi.split())
--- chương 12 tin/su_dung.py
+++
@@ -1,7 +0,0 @@
-from bai3_may_tinh.co_ban import cong, tru
-from bai3_may_tinh.nang_cao import luy_thua, can_bac_hai
-
-print("Cộng 5 + 3 =", cong(5, 3))
-print("Trừ 10 - 4 =", tru(10, 4))
-print("Lũy thừa 2^3 =", luy_thua(2, 3))
-print("Căn bậc hai của 16 =", can_bac_hai(16))
