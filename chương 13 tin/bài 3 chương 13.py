75d22bef3aeedcd8618507dc4b63eb3e552c7be3
Quann1201 <nvquan.25174600166@sv.uneti.edu.vn>
2025-12-28 18:20:32 +0700

    Delete chương 13 tin directory

--- chương 13 tin/bài 3 chương 13.py
+++
@@ -1,7 +0,0 @@
-ds_so = [1, 5, 10, 20, 25, 30]
-
-with open("so_nguyen.txt", "w", encoding="utf-8") as f:
-    for so in ds_so:
-        f.write(str(so) + "\n") 
-
-print("Đã ghi danh sách số nguyên vào tập tin so_nguyen.txt")
